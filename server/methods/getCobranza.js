04373cb90077cdfc1bdcd3038aaab37f85fb9a95
Alfonso Duarte <alfonsoduarte@gmail.com>
2017-02-16 22:42:41 -0700

    cobranza Ajuste

--- server/methods/getCobranza.js
+++ server/methods/getCobranza.js
@@ -12,12 +12,13 @@ Meteor.methods({
 			 			//Buscar si esta el Credito en el arreglo
 			 			//var buscarCredito = arreglo.indexOf(planPago.credito_id);
 			 			console.log(arreglo.length);
-			 			if (arreglo.length === undefined)
-			 			{
+			 			//if (arreglo.length === undefined)
+			 			//{
 				 				var credito = Creditos.findOne({_id: planPago.credito_id});
-				 				console.log("Credito:",credito);
+				 				//console.log("Credito:",credito);
 				 				var cliente = Meteor.users.findOne({_id: planPago.cliente_id});
-				 				console.log("cliente:",cliente);
+				 				//console.log("cliente:",cliente);
+				 				
 				 				
 				 				cobranza.credito_id = credito._id;
 				 				cobranza.cliente_id = cliente._id;
@@ -25,11 +26,21 @@ Meteor.methods({
 				 				
 				 				cobranza.importe = 0.00;
 				 				cobranza.saldo = credito.saldoActual;
-
-				 				//cobranza.recibos.push()
 				 				
-				 				//arreglo[grupo._id].
-			 			}
+				 				console.log("PlanPago:", planPago.importeRegular);
+				 				//console.log("importe:", (arreglo[credito._id].importe == undefined?0:arreglo[credito._id].importe));
+				 				
+				 				arreglo[credito._id] = {};
+				 				arreglo[credito._id].credito_id = credito._id;
+				 				arreglo[credito._id].cliente_id = cliente._id;
+				 				arreglo[credito._id].nombreCliente = cliente.profile.nombreCompleto;
+				 				if (arreglo[credito._id].importe == undefined)				 				
+				 					  arreglo[credito._id].importe = planPago.importeRegular;
+				 				else
+				 		 			  arreglo[credito._id].importe = arreglo[credito._id].importe + planPago.importeRegular;	  
+				 				
+				 				console.log("Arreglo:", arreglo);
+			 			//}
 			 			/*
 			 			else if()
 			 			{
