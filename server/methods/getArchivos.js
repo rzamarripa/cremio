4a238ee5e102dede239bbc7d5e91524ada5f7009
Jaime Ojeda <jaimeojeda@MacBook-Pro-de-Maria.local>
2017-03-14 10:22:15 -0700

    Formatos

--- server/methods/getArchivos.js
+++ server/methods/getArchivos.js
@@ -1,5 +1,6 @@
 Meteor.methods({
 		getcartaRecordatorio: function (objeto) {
+	console.log(objeto,"recordatori")
 		
 		
 		var fs = require('fs');
@@ -22,21 +23,36 @@ Meteor.methods({
 		
 		var fecha = new Date();
 		var f = fecha;
-		f = fecha.getUTCDate()+'-'+(fecha.getUTCMonth()+1)+'-'+fecha.getUTCFullYear();//+', Hora:'+fecha.getUTCHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
-		
-		doc.setData({	nombreCompleto: objeto.cliente.profile.nombreCompleto, 
-									calle: 					objeto.cliente.profile.calle,
+	    fecha = fecha.getUTCDate()+'-'+(fecha.getUTCMonth()+1)+'-'+fecha.getUTCFullYear();//+', Hora:'+fecha.getUTCHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
+	    var cliente =objeto.cliente.profile.nombreCompleto
+	    var nombreCliente  = cliente.toUpperCase();
+	    var calle = objeto.cliente.profile.calle
+	    var calleCLiente = calle.toUpperCase();
+	    var colonia = objeto.cliente.profile.colonia
+	    var coloniaCliente = colonia.toUpperCase();
+	    var ciudad = objeto.cliente.profile.ciudad
+	    var ciudadCliente = cliente.toUpperCase();
+	    var estado = objeto.cliente.profile.estado
+	    var estadoCliente = estado.toUpperCase();
+	    var pais = objeto.cliente.profile.pais
+	    var paisCliente = pais.toUpperCase();
+
+		doc.setData({		        nombreCompleto: 		nombreCliente, 
+									calle: 					calleCLiente,
 									numero: 				objeto.cliente.profile.numero,
-									colonia: 				objeto.cliente.profile.colonia,
-									codigoPostal: 	objeto.cliente.profile.codigoPostal,
-									ciudad: 				objeto.cliente.profile.ciudad,
-									estado: 				objeto.cliente.profile.estado,
-									pais: 					objeto.cliente.profile.pais,
+									colonia: 				coloniaCliente,
+									codigoPostal: 	        objeto.cliente.profile.codigoPostal,
+									ciudad: 				ciudadCliente,
+									estado: 				estadoCliente,
+									pais: 					paisCliente,
 									folio: 					objeto.credito.folio,
-									saldoTotal: 		objeto.credito.saldoTotal,
-									pagosVencidos: 	objeto.credito.pagosVencidos,
+									saldoTotal: 		    objeto.credito.saldoTotal,
+									pagosVencidos: 	        objeto.credito.pagosVencidos,
 									letra: 					objeto.credito.letra,
-									fechaEmision: 	fecha});
+									saldoActualizado:       objeto.credito.saldoActualizado,
+									pagosVencidos:          objeto.credito.pagosVencidos,
+									fechaEmision: 	        fecha});
+									
 								
 		doc.render();
  
@@ -54,5 +70,155 @@ Meteor.methods({
 		
 		
   },
+
+
+  getcartaUrgente: function (objeto) {
+	console.log(objeto,"URGENTE")
+		
+		
+		var fs = require('fs');
+    var Docxtemplater = require('docxtemplater');
+		var JSZip = require('jszip');
+		
+		var meteor_root = require('fs').realpathSync( process.cwd() + '/../' );
+		var produccion = meteor_root+"/web.browser/app/plantillas/";
+		//var produccion = "/home/isde/archivos/";
+				 
+		
+		
+		var content = fs
+    							.readFileSync(produccion+"URGENTE.docx", "binary");
+
+	  
+		var zip = new JSZip(content);
+		var doc=new Docxtemplater()
+								.loadZip(zip)
+		
+		var fecha = new Date();
+		var f = fecha;
+	    fecha = fecha.getUTCDate()+'-'+(fecha.getUTCMonth()+1)+'-'+fecha.getUTCFullYear();//+', Hora:'+fecha.getUTCHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
+	    var cliente =objeto.cliente.profile.nombreCompleto
+	    var nombreCliente  = cliente.toUpperCase();
+	    var calle = objeto.cliente.profile.calle
+	    var calleCLiente = calle.toUpperCase();
+	    var colonia = objeto.cliente.profile.colonia
+	    var coloniaCliente = colonia.toUpperCase();
+	    var ciudad = objeto.cliente.profile.ciudad
+	    var ciudadCliente = cliente.toUpperCase();
+	    var estado = objeto.cliente.profile.estado
+	    var estadoCliente = estado.toUpperCase();
+	    var pais = objeto.cliente.profile.pais
+	    var paisCliente = pais.toUpperCase();
+		
+		doc.setData({				nombreCompleto: 		nombreCliente, 
+									calle: 					calleCLiente,
+									numero: 				objeto.cliente.profile.numero,
+									colonia: 				coloniaCliente,
+									codigoPostal: 	        objeto.cliente.profile.codigoPostal,
+									ciudad: 				ciudadCliente,
+									estado: 				estadoCliente,
+									pais: 					paisCliente,
+									folio: 					objeto.credito.folio,
+									saldoTotal: 		    objeto.credito.saldoTotal,
+									pagosVencidos: 	        objeto.credito.pagosVencidos,
+									letra: 					objeto.credito.letra,
+									saldoActualizado:       objeto.credito.saldoActualizado,
+									avales:		   			objeto.credito.avales,
+									pagosVencidos:          objeto.credito.pagosVencidos,
+									fechaEmision: 	        fecha});
+									
+								
+		doc.render();
+ 
+		var buf = doc.getZip()
+             		 .generate({type:"nodebuffer"});
+ 
+		fs.writeFileSync(produccion+"URGENTESSalida.docx",buf);		
+				
+		//Pasar a base64
+		// read binary data
+    var bitmap = fs.readFileSync(produccion+"URGENTESSalida.docx");
+    
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+		
+		
+  },
+
+  getcartaCertificado: function (objeto) {
+	console.log(objeto,"certificacionPatrimonial")
+		
+		
+		var fs = require('fs');
+    	var Docxtemplater = require('docxtemplater');
+		var JSZip = require('jszip');
+		
+		var meteor_root = require('fs').realpathSync( process.cwd() + '/../' );
+		var produccion = meteor_root+"/web.browser/app/plantillas/";
+		//var produccion = "/home/isde/archivos/";
+				 
+		
+		
+		var content = fs
+    							.readFileSync(produccion+"certificacionPatrimonial.docx", "binary");
+
+	  
+		var zip = new JSZip(content);
+		var doc=new Docxtemplater()
+								.loadZip(zip)
+		
+		var fecha = new Date();
+		var f = fecha;
+	    fecha = fecha.getUTCDate()+'-'+(fecha.getUTCMonth()+1)+'-'+fecha.getUTCFullYear();//+', Hora:'+fecha.getUTCHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
+	    var cliente =objeto.cliente.profile.nombreCompleto
+	    var nombreCliente  = cliente.toUpperCase();
+	    var calle = objeto.cliente.profile.calle
+	    var calleCLiente = calle.toUpperCase();
+	    var colonia = objeto.cliente.profile.colonia
+	    var coloniaCliente = colonia.toUpperCase();
+	    var ciudad = objeto.cliente.profile.ciudad
+	    var ciudadCliente = cliente.toUpperCase();
+	    var estado = objeto.cliente.profile.estado
+	    var estadoCliente = estado.toUpperCase();
+	    var pais = objeto.cliente.profile.pais
+	    var paisCliente = pais.toUpperCase();
+
+
+		
+		doc.setData({				nombreCompleto: 		nombreCliente, 
+									calle: 					calleCLiente,
+									numero: 				objeto.cliente.profile.numero,
+									colonia: 				coloniaCliente,
+									codigoPostal: 	        objeto.cliente.profile.codigoPostal,
+									ciudad: 				ciudadCliente,
+									estado: 				estadoCliente,
+									pais: 					paisCliente,
+									folio: 					objeto.credito.folio,
+									saldoTotal: 		    objeto.credito.saldoTotal,
+									pagosVencidos: 	        objeto.credito.pagosVencidos,
+									letra: 					objeto.credito.letra,
+									saldoActualizado:       objeto.credito.saldoActualizado,
+									fechaEmision: 	        fecha,
+									avales:		   			objeto.credito.avales});
+									
+								
+		doc.render();
+ 
+		var buf = doc.getZip()
+             		 .generate({type:"nodebuffer"});
+ 
+		fs.writeFileSync(produccion+"certificacionPatrimonialSalida.docx",buf);		
+				
+		//Pasar a base64
+		// read binary data
+    var bitmap = fs.readFileSync(produccion+"certificacionPatrimonialSalida.docx");
+    
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+		
+		
+  },
+
+  	
 	
 });
