25b1baa1a9dc3849f3f2997047e4cf160a12aedc
Roberto Zamarripa <roberto@masoft.mx>
2017-04-13 10:30:19 -0600

    semanas color en cobranza

--- server/methods/getCobranza.js
+++ server/methods/getCobranza.js
@@ -3,8 +3,14 @@ Meteor.methods({
 			
 			var arreglo = {};
 			var planPagos = PlanPagos.find({fechaLimite: {$gte: fechaInicial,$lt: fechaFinal}, estatus: 0}).fetch();
-			
+			var hoy = new Date();
 			_.each(planPagos, function(planPago){
+				var classPago = "";
+					if(hoy > planPago.fechaLimite){
+						classPago = "text-danger";
+					}else{
+						classPago = "text-success";
+					}
 			 			if(arreglo[planPago.credito_id] == undefined){
 				 			
 				 			arreglo[planPago.credito_id] = {};
@@ -13,14 +19,14 @@ Meteor.methods({
 				 			arreglo[planPago.credito_id].importe = 0.00;
 				 			arreglo[planPago.credito_id].importe = planPago.importeRegular;
 				 			arreglo[planPago.credito_id].planPagos = [];
-				 			arreglo[planPago.credito_id].planPagos.push(planPago.numeroPago);
+				 			arreglo[planPago.credito_id].planPagos.push({numeroPago : planPago.numeroPago, fechaLimite : planPago.fechaLimite, classPago : classPago});
 				 			
 				 			//if (planPlago.fechaLimite )
 				 			
 			 			}else{
 				 			
 				 			arreglo[planPago.credito_id].importe += planPago.importeRegular;
-				 			arreglo[planPago.credito_id].planPagos.push(planPago.numeroPago);
+				 			arreglo[planPago.credito_id].planPagos.push({numeroPago : planPago.numeroPago, fechaLimite : planPago.fechaLimite, classPago : classPago});
 			 			}
 		
 				 		console.log("Arreglo:", _.toArray(arreglo));			
