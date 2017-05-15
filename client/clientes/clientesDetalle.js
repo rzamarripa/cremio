b240374f569c07ead174a2fbab9ef9372c74b845
Alfonso Duarte <alfonsoduarte@gmail.com>
2017-05-14 23:12:50 -0600

    ccs creditos en cliente detalle

--- client/clientes/clientesDetalle.js
+++ client/clientes/clientesDetalle.js
@@ -156,7 +156,15 @@ function ClientesDetalleCtrl($scope, $meteor, $reactive, $state, toastr, $stateP
 			return Creditos.find({estatus:2});
 		},
 		creditosPendientes : () =>{
-			return Creditos.find({estatus:1});
+			var creditos = Creditos.find({estatus:{$in:[0,1]}}).fetch();
+			if(creditos.length > 0){
+				_.each(creditos, function(credito){
+					credito.estatusClase = rc.obtenerClaseEstatus(credito.estatus);
+				})
+			}
+			
+			
+			return creditos;
 		},
 		notasCredito : () =>{
 			return NotasCredito.find({},{sort:{fecha:1}});
@@ -821,6 +829,24 @@ function ClientesDetalleCtrl($scope, $meteor, $reactive, $state, toastr, $stateP
 			$("#notaPerfil").modal('hide');
 	}
 
+	this.obtenerClaseEstatus = function(estatus){
+		console.log("hola", estatus);
+		if(estatus == 0){
+			return "danger";
+		}else if(estatus == 1){
+			return "warning";
+		}else if(estatus == 2){
+			return "primary";
+		}else if(estatus == 3){
+			return "danger";
+		}else if(estatus == 4){
+			return "info";
+		}else if(estatus == 5){
+			return "success";
+		}else if(estatus == 6){
+			return "danger";
+		}
+	}
 
 	
 }
