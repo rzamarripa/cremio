cf63c244f34355f5d9ce254f047b0091006e1606
Alfonso Duarte <alfonsoduarte@gmail.com>
2017-08-29 00:10:36 -0600

    Correciones avales, login cajeros, generador Plan, Home, detalle cliente Vista, color Rosa cancelaciones

--- client/layouts/root.js
+++ client/layouts/root.js
@@ -1,38 +1,46 @@
-angular.module("creditoMio").controller("RootCtrl", ['$scope', '$meteor', '$reactive','$state', function ($scope, $meteor, $reactive, $state)
-{
-	let root = $reactive(this).attach($scope);
-	window.root = root;
+angular.module("creditoMio")
+.controller("RootCtrl", RootCtrl);
+ function RootCtrl($scope, $meteor, $reactive,  $state) {
+
+	
+	let rc = $reactive(this).attach($scope);
+	window.rc = rc;
+	
 	this.buscar = {};
 	this.buscar.nombre = "";
 	this.buscando = false;
-	this.clientesRoot = [];
+	//this.clientesRoot = [];
 	this.clientes_ids = [];
-	this.referencias = [];
+
 	this.hoy = new Date();
-	this.caja = {};
-	this.nombreCliente = "";
-	
+	//this.caja = {};
+	//this.nombreCliente = "";
 	
 
 	this.subscribe('buscarRootClientesDistribuidores', () => {
-		if(this.getReactively("buscar.nombre").length > 4){
-			root.buscando = true;			
+		console.log(rc.getReactively("buscar.nombre"));
+		
+		if(rc.getReactively("buscar.nombre").length > 4){
+			
+			rc.buscando = true;			
 			return [{
 		    options : { limit: 20 },
 		    where : { 
-					nombreCompleto : this.getReactively('buscar.nombre')
+					nombreCompleto : rc.getReactively('buscar.nombre')
 				} 		   
 	    }];
 		}
-		else if (this.getReactively("buscar.nombre").length  == 0 )
+		else if (rc.getReactively("buscar.nombre").length  == 0 )
 			this.buscando = false;		
   });  
+
+
   
   this.helpers({
 		clientesRoot : () => {
 			
 			var clientes = Meteor.users.find({
-				"profile.nombreCompleto": { '$regex' : '.*' + this.getReactively('buscar.nombre') || '' + '.*', '$options' : 'i' },
+				"profile.nombreCompleto": { '$regex' : '.*' + rc.getReactively('buscar.nombre') || '' + '.*', '$options' : 'i' },
 		  	roles : {$in : ["Cliente", "Distribuidor"]}
 			}, { sort : {"profile.nombreCompleto" : 1 }}).fetch();
 	
@@ -49,9 +57,11 @@ angular.module("creditoMio").controller("RootCtrl", ['$scope', '$meteor', '$reac
 		},
 	});
 
-	this.verMenu =()=>{
-		var user= Meteor.user();
-		//console.log("usuario",user);
+
+	/*
+this.verMenu =()=>{
+		var user = Meteor.user();
+		console.log("usuario",user);
 		if( user && user.roles && user.roles[0]=="Cajero"){
 			
 			var caja = this.caja;
@@ -61,7 +71,9 @@ angular.module("creditoMio").controller("RootCtrl", ['$scope', '$meteor', '$reac
 		}
 		return true;
 	}
+*/
 
+/*
 	this.tieneFoto = function(foto, sexo){
 		//console.log("Aui es");
 	  if(foto === undefined){
@@ -76,6 +88,7 @@ angular.module("creditoMio").controller("RootCtrl", ['$scope', '$meteor', '$reac
 		  return foto;
 	  }
   };
+*/
  
 /*
 
@@ -91,11 +104,13 @@ angular.module("creditoMio").controller("RootCtrl", ['$scope', '$meteor', '$reac
 */
 
 	//Funcion Evalua la sessión del usuario
+	
 	this.autorun(function() {
     if(!Meteor.user()){	    
     	$state.go('anon.login');
     }    
   });	
+
 
 
 	this.descargarFormato = function() 
@@ -108,40 +123,37 @@ angular.module("creditoMio").controller("RootCtrl", ['$scope', '$meteor', '$reac
        }
        else
        {
-      function b64toBlob(b64Data, contentType, sliceSize) {
-          contentType = contentType || '';
-          sliceSize = sliceSize || 512;
-          var byteCharacters = atob(b64Data);
-          var byteArrays = [];
-          for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-            var slice = byteCharacters.slice(offset, offset + sliceSize);
-        
-            var byteNumbers = new Array(slice.length);
-            for (var i = 0; i < slice.length; i++) {
-              byteNumbers[i] = slice.charCodeAt(i);
-            }
-            var byteArray = new Uint8Array(byteNumbers);
-            byteArrays.push(byteArray);
-          }
-          var blob = new Blob(byteArrays, {type: contentType});
-          return blob;
-          }
-          var blob = b64toBlob(response, "application/docx");
-          var url = window.URL.createObjectURL(blob);
-          var dlnk = document.getElementById('dwnldLnk');
-
-           dlnk.download = "FormatoSol.docx"; 
-          dlnk.href = url;
-          dlnk.click();       
-          window.URL.revokeObjectURL(url);
-   
-      }
-    
+		      function b64toBlob(b64Data, contentType, sliceSize) {
+		          contentType = contentType || '';
+		          sliceSize = sliceSize || 512;
+		          var byteCharacters = atob(b64Data);
+		          var byteArrays = [];
+		          for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+		            var slice = byteCharacters.slice(offset, offset + sliceSize);
+		        
+		            var byteNumbers = new Array(slice.length);
+		            for (var i = 0; i < slice.length; i++) {
+		              byteNumbers[i] = slice.charCodeAt(i);
+		            }
+		            var byteArray = new Uint8Array(byteNumbers);
+		            byteArrays.push(byteArray);
+		          }
+		          var blob = new Blob(byteArrays, {type: contentType});
+		          return blob;
+		          }
+		          var blob = b64toBlob(response, "application/docx");
+		          var url = window.URL.createObjectURL(blob);
+		          var dlnk = document.getElementById('dwnldLnk');
+		
+		           dlnk.download = "FormatoSol.docx"; 
+		          dlnk.href = url;
+		          dlnk.click();       
+		          window.URL.revokeObjectURL(url);
+		   
+		      }
     });
 
   };
 
 
-
-
-}])
+};
