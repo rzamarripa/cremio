4b37414c88ada75b3892fdb9e7d75f51d99fb10c
Jaime Ojeda <jaimeojeda@MacBook-Pro-de-Maria.local>
2017-09-18 17:22:36 -0600

    horario

--- server/methods/getArchivos.js
+++ server/methods/getArchivos.js
@@ -336,8 +336,8 @@ Meteor.methods({
 		var meteor_root = require('fs').realpathSync( process.cwd() + '/../' );
 		var cmd = require('node-cmd');
 		var ImageModule = require('docxtemplater-image-module');
-		var produccion = "/home/cremio/archivos/";
-		//var produccion = meteor_root+"/web.browser/app/plantillas/";
+		//var produccion = "/home/cremio/archivos/";
+		var produccion = meteor_root+"/web.browser/app/plantillas/";
 
 
 		var opts = {}
